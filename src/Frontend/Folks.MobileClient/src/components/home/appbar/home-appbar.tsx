935631503b4077004ca8f39629aebb3f85700bfb
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-11-14 21:12:40 +0300

    Add handler for logout button press event

--- src/Frontend/Folks.MobileClient/src/components/home/appbar/home-appbar.tsx
+++ src/Frontend/Folks.MobileClient/src/components/home/appbar/home-appbar.tsx
@@ -3,16 +3,23 @@ import { useNavigation } from '@react-navigation/native';
 
 import { StackNavigation } from '../../../navigation/app-navigator';
 import UserAccount from '../../../features/users/user-account/user-account';
+import useAuth from '../../../features/auth/hooks/use-auth';
 
 const HomeAppbar = (): JSX.Element => {
   const navigation = useNavigation<StackNavigation>();
+  const { signOutAsync } = useAuth();
+
+  const handleLogoutButtonPress = async (): Promise<void> => {
+    await signOutAsync();
+    navigation.navigate('Welcome');
+  };
 
   return (
     <Appbar.Header>
       <Appbar.Content title={<UserAccount />} />
       <Appbar.Action
         icon="logout"
-        onPress={() => navigation.navigate('Welcome')}
+        onPress={handleLogoutButtonPress}
       />
     </Appbar.Header>
   );
