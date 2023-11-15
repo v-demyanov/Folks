68a762fd0e94961785b98e1f5b3dd9e588d85bf8
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-11-15 16:13:46 +0300

    Update app navigator. Defend routes by auth.

--- src/Frontend/Folks.MobileClient/src/navigation/app-navigator.tsx
+++ src/Frontend/Folks.MobileClient/src/navigation/app-navigator.tsx
@@ -4,6 +4,7 @@ import { NavigationProp } from '@react-navigation/native';
 import ChatScreen from '../screens/chat-screen/chat-screen';
 import HomeScreen from '../screens/home/home-screen';
 import WelcomeScreen from '../screens/access/welcome-screen/welcome-screen';
+import useAuth from '../features/auth/hooks/use-auth';
 
 export type RootStackParamList = {
   Home: undefined;
@@ -16,14 +17,21 @@ export type StackNavigation = NavigationProp<RootStackParamList>;
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const AppNavigator = (): JSX.Element => {
+  const { isAuthenticated } = useAuth();
+
   return (
     <Stack.Navigator
       initialRouteName="Welcome"
       screenOptions={{ headerShown: false }}
     >
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Chat" component={ChatScreen} />
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      {isAuthenticated() ? (
+        <>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Chat" component={ChatScreen} />
+        </>
+      ) : (
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      )}
     </Stack.Navigator>
   );
 };
