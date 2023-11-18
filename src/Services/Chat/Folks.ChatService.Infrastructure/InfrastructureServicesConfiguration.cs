b6b374f004af94968c5f0540a29c125481dad6df
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-11-18 21:37:52 +0300

    Add chat service database context.

---
+++ src/Services/Chat/Folks.ChatService.Infrastructure/InfrastructureServicesConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using MongoDB.Driver;
+
+using Folks.ChatService.Infrastructure.Persistence;
+
+namespace Folks.ChatService.Infrastructure;
+
+public static class InfrastructureServicesConfiguration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.ConfigureMongoDatabase(configuration);
+
+        return services;
+    }
+
+    private static IServiceCollection ConfigureMongoDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration
+            .GetValue<string>("MongoDbConfig:ConnectionString");
+
+        var databaseName = configuration
+            .GetValue<string>("MongoDbConfig:DatabaseName");
+
+        if (connectionString is null)
+        {
+            throw new NullReferenceException(nameof(connectionString));
+        }
+
+        if (databaseName is null)
+        {
+            throw new NullReferenceException(nameof(databaseName));
+        }
+
+        var mongoClient = new MongoClient(connectionString);
+        var mongoDatabase = mongoClient.GetDatabase(databaseName);
+
+        services.AddDbContext<ChatServiceDbContext>(options =>
+            options.UseMongoDB(mongoDatabase.Client, mongoDatabase.DatabaseNamespace.DatabaseName));
+
+        return services;
+    }
+}
---
+++ src/Services/Chat/Folks.ChatService.Infrastructure/Persistence/ChatServiceDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using MongoDB.EntityFrameworkCore.Extensions;
+
+using Folks.ChatService.Domain.Entities;
+
+namespace Folks.ChatService.Infrastructure.Persistence;
+
+public class ChatServiceDbContext : DbContext
+{
+    public ChatServiceDbContext(DbContextOptions<ChatServiceDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Chat> Chats => Set<Chat>();
+
+    public DbSet<Group> Groups => Set<Group>();
+
+    public DbSet<Message> Messages => Set<Message>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Chat>().ToCollection("chats");
+        modelBuilder.Entity<Group>().ToCollection("groups");
+        modelBuilder.Entity<Message>().ToCollection("messages");
+    }
+}
