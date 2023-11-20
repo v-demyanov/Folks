21b714d52e83871193a41cee15f5649755e05a64
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-11-20 19:29:34 +0300

    Update login input model. Add password string length attribute.

--- src/Services/Identity/Folks.IdentityService.Api/Pages/Account/Login/InputModel.cs
+++ src/Services/Identity/Folks.IdentityService.Api/Pages/Account/Login/InputModel.cs
@@ -7,5 +7,6 @@ public record class InputModel
 
     [Required]
     [DataType(DataType.Password)]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
     public required string Password { get; init; }
 }
