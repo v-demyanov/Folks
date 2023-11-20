13a5573efe5e55a73980b1970dd383cce962c06b
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-11-20 19:30:43 +0300

    Update login page script. Move setting field validation style in separated function.

--- src/Services/Identity/Folks.IdentityService.Api/Pages/Account/Login/Index.cshtml.js
+++ src/Services/Identity/Folks.IdentityService.Api/Pages/Account/Login/Index.cshtml.js
@@ -1,48 +1,56 @@
-﻿const usernameField = document.getElementById('Input_UserName');
+const usernameField = document.getElementById('Input_UserName');
 const passwordField = document.getElementById('Input_Password');
+
 const usernameInvalidTooltip = document.getElementById('username-invalid-tooltip');
 const passwordInvalidTooltip = document.getElementById('password-invalid-tooltip');
+
 const form = document.getElementById('login-form');
 const submitBtn = document.getElementById('submit-btn');
 
 usernameField.addEventListener('input', () => {
-    const isValid = usernameField.checkValidity();
+  const isValid = usernameField.checkValidity();
 
-    if (isValid) {
-        usernameField.classList.add('is-valid');
-        usernameField.classList.remove('is-invalid');
-    } else {
-        usernameField.classList.add('is-invalid');
-        usernameField.classList.remove('is-valid');
-    }
+  if (isValid) {
+    setFieldValidStyle(usernameField);
+  } else {
+    setFieldInvalidStyle(usernameField);
+  }
 });
 
 passwordField.addEventListener('input', () => {
-    const isValid = passwordField.checkValidity();
+  const isValid = passwordField.checkValidity();
 
-    if (isValid) {
-        passwordField.classList.add('is-valid');
-        passwordField.classList.remove('is-invalid');
-    } else {
-        passwordField.classList.add('is-invalid');
-        passwordField.classList.remove('is-valid');
-    }
+  if (isValid) {
+    setFieldValidStyle(passwordField);
+  } else {
+    setFieldInvalidStyle(passwordField);
+  }
 });
 
 usernameField.addEventListener('invalid', (event) => {
-    usernameInvalidTooltip.innerHTML = event.target.validationMessage;
+  usernameInvalidTooltip.innerHTML = event.target.validationMessage;
 });
 
 passwordField.addEventListener('invalid', (event) => {
-    passwordInvalidTooltip.innerHTML = event.target.validationMessage;
+  passwordInvalidTooltip.innerHTML = event.target.validationMessage;
 });
 
 form.addEventListener('input', () => {
-    const isValid = form.checkValidity();
+  const isValid = form.checkValidity();
 
-    if (isValid) {
-        submitBtn.disabled = false;
-    } else {
-        submitBtn.disabled = true;
-    }
+  if (isValid) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
 });
+
+function setFieldInvalidStyle(field) {
+  field.classList.add('is-invalid');
+  field.classList.remove('is-valid');
+}
+
+function setFieldValidStyle(field) {
+  field.classList.add('is-valid');
+  field.classList.remove('is-invalid');
+}
