59941bea3a47483fe3ef094152817f85a99191d4
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-11-22 02:40:21 +0300

    Add user registered event.

---
+++ src/Common/Folks.EventBus.Messages/IdentityService/UserRegistered.cs
@@ -0,0 +1,10 @@
+﻿namespace Folks.EventBus.Messages.IdentityService;
+
+public record UserRegistered
+{
+    public required string UserId { get; init; }
+
+    public required string UserName { get; init; }
+
+    public required string Email { get; init; }
+}
