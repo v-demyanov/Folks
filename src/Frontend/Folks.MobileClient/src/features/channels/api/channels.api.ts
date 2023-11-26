849d5426483bec59fb0ec7010e2e1aa4fbf604ba
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-11-26 22:30:07 +0300

    Add channels api (client).

---
+++ src/Frontend/Folks.MobileClient/src/features/channels/api/channels.api.ts
@@ -0,0 +1,13 @@
+import { api } from '../../../common/api/api';
+import IChannel from '../models/channel';
+
+const channelsApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getOwnChannels: builder.query<IChannel[], null>({
+      query: () => ({ url: '/chatservice/channels', method: 'GET' }),
+    }),
+  }),
+  overrideExisting: false,
+});
+
+export const { useGetOwnChannelsQuery } = channelsApi;
