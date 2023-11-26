90172eb68cb7a22e1947c0057d6c65db5a153a52
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-11-26 22:27:30 +0300

    Add redux store.

--- src/Frontend/Folks.MobileClient/App.tsx
+++ src/Frontend/Folks.MobileClient/App.tsx
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { useColorScheme } from 'react-native';
+import { Provider } from 'react-redux';
 
 import { darkTheme, lightTheme } from './src/common/themes/themes';
 import AppNavigator from './src/navigation/app-navigator';
 import { AuthProvider } from './src/features/auth/context/auth-provider';
+import { store } from './src/common/store/store';
 
 export default function App() {
   const colorScheme = useColorScheme();
@@ -16,8 +18,10 @@ export default function App() {
     <NavigationContainer>
       <PaperProvider theme={theme}>
         <AuthProvider>
-          <AppNavigator />
-          <StatusBar style="auto" />
+          <Provider store={store}>
+            <AppNavigator />
+            <StatusBar style="auto" />
+          </Provider>
         </AuthProvider>
       </PaperProvider>
     </NavigationContainer>
---
+++ src/Frontend/Folks.MobileClient/src/common/api/api.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { TokenResponse } from 'expo-auth-session';
+import * as SecureStore from 'expo-secure-store';
+
+import { TOKEN_RESPONSE_KEY } from '../constants/secure-store-keys.constants';
+
+export const api = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.EXPO_PUBLIC_API_GTW_URL,
+    prepareHeaders: async (headers) => {
+      const rawTokenResponse = await SecureStore.getItemAsync(TOKEN_RESPONSE_KEY);
+
+      if (rawTokenResponse) {
+        const tokenResponse = JSON.parse(rawTokenResponse) as (TokenResponse | undefined);
+        headers.set('authorization', `Bearer ${tokenResponse?.accessToken}`);
+      }
+
+      return headers;
+    },
+  }),
+  endpoints: () => ({}),
+});
