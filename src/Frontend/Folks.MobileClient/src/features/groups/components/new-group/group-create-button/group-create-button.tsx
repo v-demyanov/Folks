996d018e4f297c874da677877cc77ac7abed395a
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-11-29 00:53:27 +0300

    Add group create button.

---
+++ src/Frontend/Folks.MobileClient/src/features/groups/components/new-group/group-create-button/group-create-button.tsx
@@ -0,0 +1,13 @@
+import { FAB } from 'react-native-paper';
+
+const GroupCreateButton = (): JSX.Element => {
+  return (
+    <FAB
+      icon="check"
+      style={{ position: 'absolute', bottom: 16, right: 16 }}
+      onPress={() => {}}
+    />
+  );
+};
+
+export default GroupCreateButton;
