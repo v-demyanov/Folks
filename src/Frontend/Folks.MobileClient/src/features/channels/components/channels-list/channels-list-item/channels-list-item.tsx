3e44e746f839434d95148a37aa8f7c29f96c8605
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-11-29 01:02:01 +0300

    Fix channels list item.

--- src/Frontend/Folks.MobileClient/src/features/channels/components/channels-list/channels-list-item/channels-list-item.tsx
+++ src/Frontend/Folks.MobileClient/src/features/channels/components/channels-list/channels-list-item/channels-list-item.tsx
@@ -1,7 +1,7 @@
 import { Avatar, Badge, List, Text } from 'react-native-paper';
 import { View } from 'react-native';
 
-import { CHANNELS_LIST_ITEM_IMAGE_SIZE } from '../../../../../common/constants/icons.constants';
+import { LIST_ITEM_IMAGE_SIZE } from '../../../../../common/constants/icons.constants';
 import buildStyles from './channels-list-item.styles';
 import IChannel from '../../../models/channel';
 import { getUserFrendlyDateString } from '../../../../../common/helpers/date-helpers';
@@ -17,7 +17,7 @@ const ChannelsListItem = ({ channel }: { channel: IChannel }): JSX.Element => {
       description={'Last message...'}
       onPress={() => {}}
       left={() => (
-        <Avatar.Icon size={CHANNELS_LIST_ITEM_IMAGE_SIZE} icon="account" />
+        <Avatar.Icon size={LIST_ITEM_IMAGE_SIZE} icon="account" />
       )}
       right={() => (
         <View style={[styles.view]}>
