c4cfd2704a17ab0feea7b61942e74d5eaae94fe1
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-11-29 00:56:19 +0300

    Add new group header.

---
+++ src/Frontend/Folks.MobileClient/src/features/groups/components/new-group/new-group-header/new-group-header.tsx
@@ -0,0 +1,17 @@
+import { useNavigation } from '@react-navigation/native';
+import { Appbar } from 'react-native-paper';
+
+import { StackNavigation } from '../../../../../navigation/app-navigator';
+
+const NewGroupHeader = (): JSX.Element => {
+  const navigation = useNavigation<StackNavigation>();
+
+  return (
+    <Appbar.Header>
+      <Appbar.BackAction onPress={() => navigation.goBack()} />
+      <Appbar.Content title="New group" titleStyle={{fontWeight: 'bold'}} />
+    </Appbar.Header>
+  );
+};
+
+export default NewGroupHeader;
