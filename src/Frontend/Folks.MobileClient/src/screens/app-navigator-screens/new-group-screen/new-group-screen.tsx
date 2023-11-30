0065c09cb9d7acfa80bb70dde6ca24008c4a6b22
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-12-01 00:23:14 +0300

    Update new group screen. Replace mock data on data from api.

--- src/Frontend/Folks.MobileClient/src/screens/app-navigator-screens/new-group-screen/new-group-screen.tsx
+++ src/Frontend/Folks.MobileClient/src/screens/app-navigator-screens/new-group-screen/new-group-screen.tsx
@@ -1,39 +1,55 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import NewGroupForm from '../../../features/groups/components/new-group/new-group-form/new-group-form';
 import NewGroupHeader from '../../../features/groups/components/new-group/new-group-header/new-group-header';
 import SelectableUsersList from '../../../features/users/components/selectable-users-list/selectable-users-list';
-import IUser from '../../../features/users/models/user';
 import SelectedUsersChips from '../../../features/users/components/selected-users-chips/selected-users-chips';
 import GroupCreateButton from '../../../features/groups/components/new-group/group-create-button/group-create-button';
+import { useGetAllUsersQuery } from '../../../features/users/api/users.api';
+import ISelectableUser from '../../../features/users/models/selectable-user';
+import useAuth from '../../../features/auth/hooks/use-auth';
 
 const NewGroupScreen = (): JSX.Element => {
-  const [users, setUsers] = useState(
-    [...Array(20).keys()].map((x) => ({
-      id: x.toString(),
-      title: `User ${x}`,
-      selected: false,
-      status: 'last seen recently',
-    }))
-  );
+  const [selectableUsers, setSelectableUsers] = useState<ISelectableUser[]>([]);
+  const { currentUser } = useAuth();
+  const { data: users = [], isSuccess: isUsersQuerySuccess } =
+    useGetAllUsersQuery(null, { refetchOnMountOrArgChange: true });
 
-  const handleListItemPress = (user: IUser) => {
-    user.selected = !user.selected;
-    setUsers([...users]);
+  useEffect(() => {
+    const selectableUsers = prepareSelecatableUsers();
+    setSelectableUsers(selectableUsers);
+  }, [isUsersQuerySuccess]);
+
+  const handleListItemPress = (user: ISelectableUser) => {
+    user.isSelected = !user.isSelected;
+    setSelectableUsers([...selectableUsers]);
   };
 
-  const handleChipClose = (user: IUser) => {
-    user.selected = false;
-    setUsers([...users]);
+  const handleChipClose = (user: ISelectableUser) => {
+    user.isSelected = false;
+    setSelectableUsers([...selectableUsers]);
   };
+
+  function prepareSelecatableUsers(): ISelectableUser[] {
+    return users
+      .filter((user) => user.id !== currentUser?.sub)
+      .map((user) => ({
+        ...user,
+        isSelected: false,
+        status: 'last seen recently',
+      }));
+  }
 
   return (
     <>
       <NewGroupHeader />
-      <NewGroupForm users={users} />
-      <SelectedUsersChips users={users} onChipClose={handleChipClose} />
+      <NewGroupForm users={selectableUsers} />
+      <SelectedUsersChips
+        users={selectableUsers}
+        onChipClose={handleChipClose}
+      />
       <SelectableUsersList
-        users={users}
+        users={selectableUsers}
         onListItemPress={handleListItemPress}
       />
       <GroupCreateButton />
