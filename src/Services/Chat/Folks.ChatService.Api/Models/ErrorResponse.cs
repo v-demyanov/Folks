883371882b1eab97920143f30296bd4ce877c792
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-12-03 16:39:46 +0300

    Update chat service api. Add error response.

---
+++ src/Services/Chat/Folks.ChatService.Api/Models/ErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Folks.ChatService.Api.Models;
+
+public record class ErrorResponse
+{
+    public int StatusCode { get; init; }
+
+    public required string Title { get; init; }
+
+    public IDictionary<string, string[]> Errors { get; init; } = new Dictionary<string, string[]>();
+}
