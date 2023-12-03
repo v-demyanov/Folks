c4269e581e0d007465367f62ebc0f9735fcedf5a
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-12-03 16:53:11 +0300

    Update chat service. Add channels controller and group controller api route patterns.

--- src/Services/Chat/Folks.ChatService.Api/Constants/ApiRoutePatterns.cs
+++ src/Services/Chat/Folks.ChatService.Api/Constants/ApiRoutePatterns.cs
@@ -2,7 +2,12 @@
 
 public static class ApiRoutePatterns
 {
+    public const string BaseRoute = "api/v1/";
+
+    public const string ChannelsController = $"{BaseRoute}channels";
+
+    public const string GroupsController = $"{ChannelsController}/groups";
+
     public const string ChannelsHub = $"{BaseRoute}hubs/channels";
 
-    public const string BaseRoute = "api/v1/";
 }
