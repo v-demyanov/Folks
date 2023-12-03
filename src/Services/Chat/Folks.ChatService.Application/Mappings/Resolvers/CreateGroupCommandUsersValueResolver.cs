b19ccbc83e931fedb2935cdaa70a8ce7ef8d5f83
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-12-03 16:56:14 +0300

    Update chat service. Add value resolver for users field in CreateGroupCommand.

---
+++ src/Services/Chat/Folks.ChatService.Application/Mappings/Resolvers/CreateGroupCommandUsersValueResolver.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+
+using MongoDB.Bson;
+using Folks.ChatService.Domain.Entities;
+using Folks.ChatService.Infrastructure.Persistence;
+using Microsoft.Extensions.Logging;
+using Folks.ChatService.Application.Features.Groups.Commands.CreateGroupCommand;
+
+namespace Folks.ChatService.Application.Mappings.Resolvers;
+
+public class CreateGroupCommandUsersValueResolver : IValueResolver<CreateGroupCommand, Group, ICollection<ObjectId>>
+{
+    private readonly ChatServiceDbContext _dbContext;
+    private readonly ILogger<CreateGroupCommandUsersValueResolver> _logger;
+
+    public CreateGroupCommandUsersValueResolver(ChatServiceDbContext dbContext, ILogger<CreateGroupCommandUsersValueResolver> logger)
+    {
+        _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        _logger = logger;
+    }
+
+    public ICollection<ObjectId> Resolve(CreateGroupCommand source, Group destination, ICollection<ObjectId> destMember, ResolutionContext context)
+    {
+        var users = _dbContext.Users
+            .Where(user => source.UserIds.Any(userId => userId == user.SourceId))
+            .ToList();
+
+        return users.Select(user => user.Id).ToList();
+    }
+}
