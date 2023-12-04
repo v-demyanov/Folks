2c50043c01bdf07963866575fa483e66e3a4abe6
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-12-05 00:22:47 +0300

    Update chat service. Remove redundant code.

--- src/Services/Chat/Folks.ChatService.Application/Mappings/Resolvers/CreateGroupCommandUsersValueResolver.cs
+++ src/Services/Chat/Folks.ChatService.Application/Mappings/Resolvers/CreateGroupCommandUsersValueResolver.cs
@@ -1,9 +1,9 @@
 ﻿using AutoMapper;
 
 using MongoDB.Bson;
+
 using Folks.ChatService.Domain.Entities;
 using Folks.ChatService.Infrastructure.Persistence;
-using Microsoft.Extensions.Logging;
 using Folks.ChatService.Application.Features.Groups.Commands.CreateGroupCommand;
 
 namespace Folks.ChatService.Application.Mappings.Resolvers;
@@ -11,12 +11,10 @@ namespace Folks.ChatService.Application.Mappings.Resolvers;
 public class CreateGroupCommandUsersValueResolver : IValueResolver<CreateGroupCommand, Group, ICollection<ObjectId>>
 {
     private readonly ChatServiceDbContext _dbContext;
-    private readonly ILogger<CreateGroupCommandUsersValueResolver> _logger;
 
-    public CreateGroupCommandUsersValueResolver(ChatServiceDbContext dbContext, ILogger<CreateGroupCommandUsersValueResolver> logger)
+    public CreateGroupCommandUsersValueResolver(ChatServiceDbContext dbContext)
     {
         _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
-        _logger = logger;
     }
 
     public ICollection<ObjectId> Resolve(CreateGroupCommand source, Group destination, ICollection<ObjectId> destMember, ResolutionContext context)
