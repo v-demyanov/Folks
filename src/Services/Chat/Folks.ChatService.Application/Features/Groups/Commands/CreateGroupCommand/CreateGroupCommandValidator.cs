866bc3c5ea2a7971b758991b6c0ef407f89e1522
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-12-05 00:25:53 +0300

    Update chat service. Update CreateGroupCommandValidator: add rule for UserIds.

--- src/Services/Chat/Folks.ChatService.Application/Features/Groups/Commands/CreateGroupCommand/CreateGroupCommandValidator.cs
+++ src/Services/Chat/Folks.ChatService.Application/Features/Groups/Commands/CreateGroupCommand/CreateGroupCommandValidator.cs
@@ -1,16 +1,21 @@
 ﻿using FluentValidation;
 
 using Folks.ChatService.Domain.Constants;
+using Folks.ChatService.Infrastructure.Persistence;
 
 namespace Folks.ChatService.Application.Features.Groups.Commands.CreateGroupCommand;
 
 public class CreateGroupCommandValidator : AbstractValidator<CreateGroupCommand>
 {
-    public CreateGroupCommandValidator()
+    public CreateGroupCommandValidator(ChatServiceDbContext dbContext)
     {
         RuleFor(command => command.Title)
             .NotEmpty()
             .MinimumLength(GroupSettings.TitleMinimumLength)
             .MaximumLength(GroupSettings.TitleMaximumLength);
+
+        RuleFor(command => command.UserIds)
+            .Must(userIds => userIds.Except(dbContext.Users.ToList().Select(user => user.SourceId)).Count() == 0)
+            .WithMessage("Some users don't exist.");
     }
 }
