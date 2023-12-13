ff511c019ea6d65a0893f219d25aa69c394b0431
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-12-13 15:54:30 +0300

    Update mobile client. Update icons constants.

--- src/Frontend/Folks.MobileClient/src/common/constants/icons.constants.ts
+++ src/Frontend/Folks.MobileClient/src/common/constants/icons.constants.ts
@@ -4,4 +4,6 @@ export const USER_ACCOUNT_IMAGE_SIZE: number = 35;
 
 export const LIST_ITEM_IMAGE_SIZE: number = 50;
 
-export const CHAT_SEND_MESSAGE_BUTTON_ICON_SIZE: number = 25;
+export const SEND_MESSAGE_BUTTON_ICON_SIZE: number = 25;
+
+export const GROUP_ICON_SIZE: number = 45;
--- src/Frontend/Folks.MobileClient/src/features/chats/chat-footer/message-send-button/message-send-button.tsx
+++ src/Frontend/Folks.MobileClient/src/features/chats/chat-footer/message-send-button/message-send-button.tsx
@@ -1,12 +1,12 @@
 import { IconButton } from 'react-native-paper';
 
-import { CHAT_SEND_MESSAGE_BUTTON_ICON_SIZE } from '../../../../common/constants/icons.constants';
+import { SEND_MESSAGE_BUTTON_ICON_SIZE } from '../../../../common/constants/icons.constants';
 
 const MessageSendButton = (): JSX.Element => {
   return (
     <IconButton
       icon="send"
-      size={CHAT_SEND_MESSAGE_BUTTON_ICON_SIZE}
+      size={SEND_MESSAGE_BUTTON_ICON_SIZE}
       onPress={() => {}}
     />
   );
