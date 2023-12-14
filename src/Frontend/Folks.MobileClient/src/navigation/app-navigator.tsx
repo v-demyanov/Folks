65da139828792e015317716621bece208448ea10
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-12-15 00:46:13 +0300

    Update mobile client. Remove redundant chat components.

--- src/Frontend/Folks.MobileClient/src/navigation/app-navigator.tsx
+++ src/Frontend/Folks.MobileClient/src/navigation/app-navigator.tsx
@@ -1,7 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationProp } from '@react-navigation/native';
 
-import ChatScreen from '../screens/app-navigator-screens/chat-screen/chat-screen';
 import HomeScreen from '../screens/app-navigator-screens/home-screen/home-screen';
 import WelcomeScreen from '../screens/app-navigator-screens/welcome-screen/welcome-screen';
 import useAuth from '../features/auth/hooks/use-auth';
@@ -11,7 +10,6 @@ import IChannel from '../features/channels/models/channel';
 
 export type RootStackParamList = {
   Home: undefined;
-  Chat: undefined;
   Welcome: undefined;
   CreateGroup: undefined;
   Group: IChannel;
@@ -32,7 +30,6 @@ const AppNavigator = (): JSX.Element => {
       {isAuthenticated() ? (
         <>
           <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Chat" component={ChatScreen} />
           <Stack.Screen name="CreateGroup" component={CreateGroupScreen} />
           <Stack.Screen name="Group" component={GroupScreen} />
         </>
