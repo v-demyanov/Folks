64f4e4b029fd35e7e9599438b97dcf9569d19665
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-12-19 17:59:11 +0300

    Update channels service. Add validator for create message command.

---
+++ src/Services/Channels/Folks.ChannelsService.Application/Features/Messages/Commands/CreateMessageCommand/CreateMessageCommandValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+
+using Folks.ChannelsService.Application.Features.Channels.Enums;
+using Folks.ChannelsService.Domain.Constants;
+using Folks.ChannelsService.Infrastructure.Persistence;
+
+namespace Folks.ChannelsService.Application.Features.Messages.Commands.CreateMessageCommand;
+
+public class CreateMessageCommandValidator : AbstractValidator<CreateMessageCommand>
+{
+    public CreateMessageCommandValidator(ChatServiceDbContext dbContext)
+    {
+        RuleFor(query => query.OwnerId)
+            .NotEmpty();
+
+        RuleFor(query => query.OwnerId)
+            .Must(ownerId => dbContext.Users.Any(user => user.SourceId == ownerId))
+            .WithMessage(query => $"The user with id=\"{query.OwnerId}\" doesn't exist.");
+
+        RuleFor(query => query.ChannelId)
+            .NotEmpty();
+
+        RuleFor(query => query.ChannelId)
+            .Must((command, channelId) =>
+                command.ChannelType switch
+                {
+                    ChannelType.Chat => dbContext.Chats.Any(chat => chat.Id.ToString() == channelId),
+                    ChannelType.Group => dbContext.Groups.Any(group => group.Id.ToString() == channelId),
+                    _ => false,
+                })
+            .WithMessage(query => $"The channel with id=\"{query.ChannelId}\" doesn't exist.");
+
+        RuleFor(command => command.Content)
+            .NotEmpty()
+            .MaximumLength(MessageSettings.ContentMaximumLength);
+    }
+}
---
+++ src/Services/Channels/Folks.ChannelsService.Domain/Constants/MessageSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace Folks.ChannelsService.Domain.Constants;
+
+public static class MessageSettings
+{
+    public const int ContentMaximumLength = 4096;
+}
