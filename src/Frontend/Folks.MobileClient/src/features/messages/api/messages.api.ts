4820beb5f9df9c9a56013b5b6106a4c116ae33a1
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-12-20 21:47:07 +0300

    Update mobile client. Add get messages endpoint.

--- src/Frontend/Folks.MobileClient/src/features/messages/api/messages.api.ts
+++ src/Frontend/Folks.MobileClient/src/features/messages/api/messages.api.ts
@@ -3,9 +3,33 @@ import { HttpStatusCode } from 'axios';
 import { api } from '../../../api/api';
 import { channelsHubConnection } from '../../signalr/connections';
 import ICreateMessageCommand from '../models/create-message-command';
+import IGetMessagesQuery from '../models/get-messages-query';
+import IMessage from '../models/message';
 
 const messagesApi = api.injectEndpoints({
   endpoints: (builder) => ({
+    getMessages: builder.query<IMessage[], IGetMessagesQuery>({
+      query: (arg) => ({
+        url: `/channelsservice/channels/${arg.channelId}/messages?channelType=${arg.channelType}`,
+        method: 'GET',
+      }),
+      onCacheEntryAdded: async (
+        arg,
+        { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
+      ) => {
+        try {
+          await cacheDataLoaded;
+
+          channelsHubConnection.on('ReceiveMessage', (message: IMessage) => {
+            updateCachedData((draft) => {
+              draft.push(message);
+            });
+          });
+        } catch {}
+
+        await cacheEntryRemoved;
+      },
+    }),
     sendMessage: builder.mutation<null, ICreateMessageCommand>({
       queryFn: async (arg) => {
         try {
@@ -27,4 +51,4 @@ const messagesApi = api.injectEndpoints({
   overrideExisting: false,
 });
 
-export const { useSendMessageMutation } = messagesApi;
+export const { useGetMessagesQuery, useSendMessageMutation } = messagesApi;
