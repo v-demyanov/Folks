8a3bc0a9d4be333ee79378dee9ace562c4d57cf2
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2023-12-20 21:50:34 +0300

    Update mobile client. Add updating getOwnChannels cache on ReceiveChannel event.

--- src/Frontend/Folks.MobileClient/src/features/channels/api/channels.api.ts
+++ src/Frontend/Folks.MobileClient/src/features/channels/api/channels.api.ts
@@ -1,10 +1,27 @@
 import { api } from '../../../api/api';
+import { channelsHubConnection } from '../../signalr/connections';
 import IChannel from '../models/channel';
 
 const channelsApi = api.injectEndpoints({
   endpoints: (builder) => ({
     getOwnChannels: builder.query<IChannel[], null>({
       query: () => ({ url: '/channelsservice/channels', method: 'GET' }),
+      onCacheEntryAdded: async (
+        arg,
+        { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
+      ) => {
+        try {
+          await cacheDataLoaded;
+
+          channelsHubConnection.on('ReceiveChannel', (channel: IChannel) => {
+            updateCachedData((draft) => {
+              draft.push(channel);
+            });
+          });
+        } catch {}
+
+        await cacheEntryRemoved;
+      },
     }),
   }),
   overrideExisting: false,
