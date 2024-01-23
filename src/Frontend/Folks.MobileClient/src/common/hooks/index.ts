aac1e205400df4ceef723dd2bef1f68b3ff6b6a5
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2024-01-23 23:41:59 +0300

    Update mobile client. Add the useSet hook.

--- src/Frontend/Folks.MobileClient/src/common/hooks/index.ts
+++ src/Frontend/Folks.MobileClient/src/common/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from './store-hooks';
 export { default as useArrayEffect } from './use-array-effect';
 export { default as useSecureStore } from './use-secure-store';
+export { default as useSet } from './use-set';
---
+++ src/Frontend/Folks.MobileClient/src/common/hooks/use-set.ts
@@ -0,0 +1,42 @@
+import { useMemo, useRef, useState } from 'react';
+
+export default function useSet<T>(initialValue: T[]): Set<T> {
+  const triggerRender = useState(0)[1];
+  const set = useRef<Set<T>>(new Set<T>(initialValue));
+
+  return useMemo<Set<T>>(
+    (): Set<T> =>
+      ({
+        add(item): void {
+          if (set.current.has(item)) {
+            return;
+          }
+          set.current.add(item);
+          triggerRender((i) => ++i);
+        },
+        delete(item): void {
+          if (!set.current.has(item)) {
+            return;
+          }
+          set.current.delete(item);
+          triggerRender((i) => ++i);
+        },
+        clear(): void {
+          if (set.current.size === 0) {
+            return;
+          }
+          set.current.clear();
+          triggerRender((i) => ++i);
+        },
+        has: (item: T): boolean => set.current.has(item),
+        keys: (): IterableIterator<T> => set.current.keys(),
+        values: (): IterableIterator<T> => set.current.values(),
+        forEach: (...args): void => set.current.forEach(...args),
+        [Symbol.iterator]: (): IterableIterator<T> => set.current.values(),
+        get size(): number {
+          return set.current.size;
+        },
+      } as Set<T>),
+    []
+  );
+}
