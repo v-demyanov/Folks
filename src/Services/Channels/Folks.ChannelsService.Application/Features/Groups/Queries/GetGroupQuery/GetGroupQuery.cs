66be1b5711efc41cd72286062df2c00e559811a1
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2024-02-01 17:55:36 +0300

    feat(request): add GetGroupQuery and its validator
    Refs: #2

---
+++ src/Services/Channels/Folks.ChannelsService.Application/Features/Groups/Queries/GetGroupQuery/GetGroupQuery.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) v-demyanov. All rights reserved.
+
+using Folks.ChannelsService.Application.Features.Groups.Common.Dto;
+
+using MediatR;
+
+namespace Folks.ChannelsService.Application.Features.Groups.Queries.GetGroupQuery;
+
+public class GetGroupQuery : IRequest<GroupDto>
+{
+    required public string GroupId { get; init; }
+
+    required public string CurrentUserId { get; init; }
+}
---
+++ src/Services/Channels/Folks.ChannelsService.Application/Features/Groups/Queries/GetGroupQuery/GetGroupQueryValidator.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) v-demyanov. All rights reserved.
+
+using FluentValidation;
+
+using Folks.ChannelsService.Application.Common.Models;
+using Folks.ChannelsService.Application.Extensions;
+using Folks.ChannelsService.Application.Features.Channels.Common.Enums;
+using Folks.ChannelsService.Infrastructure.Persistence;
+
+namespace Folks.ChannelsService.Application.Features.Groups.Queries.GetGroupQuery;
+
+public class GetGroupQueryValidator : AbstractValidator<GetGroupQuery>
+{
+    public GetGroupQueryValidator(ChannelsServiceDbContext dbContext)
+    {
+        this.RuleFor(query => query.CurrentUserId)
+            .NotEmpty();
+
+        this.RuleFor(query => query.CurrentUserId)
+            .UserMustExist(dbContext);
+
+        this.RuleFor(query => query.CurrentUserId)
+            .Must((query, currentUserId) => dbContext.Users
+                .GetByGroupId(query.GroupId)
+                .Any(user => user.SourceId == currentUserId))
+            .WithMessage(query => $"User with id=\"{query.CurrentUserId}\" haven't joined this group.");
+
+        this.RuleFor(query => query.GroupId)
+            .NotEmpty();
+
+        this.RuleFor(query => new ChannelMustExistCustomValidatorProperty
+        {
+            ChannelId = query.GroupId,
+            ChannelType = ChannelType.Group,
+        }).ChannelMustExist(dbContext);
+    }
+}
