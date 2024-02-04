f146beb4be235a57d508d1a6b3680e11bce12a62
Vladislav Demyanov <vl4d.demyanov@yandex.ru>
2024-02-05 01:05:34 +0300

    feat(integrations tests): add GetGroupEndpointTests
    Refs: #2

---
+++ src/Services/Channels/Tests/Folks.ChannelsService.IntegrationTests/Factories/GroupsControllerClientFactory.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) v-demyanov. All rights reserved.
+
+using Folks.ChannelsService.IntegrationTests.Clients;
+using Folks.ChannelsService.IntegrationTests.Factories.Contracts;
+
+namespace Folks.ChannelsService.IntegrationTests.Factories;
+
+public class GroupsControllerClientFactory : ControllerClientFactory<GroupsControllerClient>
+{
+    public GroupsControllerClientFactory(ChannelsWebApplicationFactory webApplicationFactory)
+        : base(webApplicationFactory)
+    {
+    }
+
+    protected override GroupsControllerClient CreateControllerClient(HttpClient httpClient) =>
+        new (httpClient);
+}
